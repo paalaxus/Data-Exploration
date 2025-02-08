86889218f2bb361cf4b92ce690133e90cc6d88f7
Paa kofi Boafo <58373408+paalaxus@users.noreply.github.com>
2025-02-08 13:40:25 -0600

    Update airbnb.py

--- airbnb.py
+++ airbnb.py
@@ -2,11 +2,9 @@
 from tkinter.messagebox import ABORT
 
 #read data
-
 import pandas as pd
 
 #visualize data
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
@@ -15,21 +13,23 @@ import plotly.express as px
 plt.rcParams['figure.figsize'] = [8,4]
 sns.set_theme(style = "darkgrid")
 
-listings = pd.read_csv("/Users/ra4284de/Downloads/PROJECTS/AB_NYC_2019.csv")
-#print(listings.shape)
+listings = pd.read_csv("/Users/hostname/Downloads/PROJECTS/AB_NYC_2019.csv")
+print(listings.shape) #obtain the shape of the dataset
 #print(listings.head(4))
 
 
 #explore data
-#print(listings.columns)
-groupbyneighbourhood = listings.groupby(['neighbourhood'],as_index= False)['id'].count() #counting each time a neighbourhood shows
+#print(listings.columns) to get column names
+
+groupbyneighbourhood = listings.groupby(['neighbourhood'],as_index= False)['id'].count() 
+#counting each time a neighbourhood shows
+
 print(groupbyneighbourhood)
 top10= groupbyneighbourhood.sort_values(['id'],ascending=False).head(10)
-print("next part")
+print("____________________") 
 print(top10)
-#print(groupbyneighbourhood.head(4))"""
 
 
 #visualization
 sns.barplot(data=top10,y="neighbourhood",x='id').set_title('Top 10 Bookings')
-plt.show()
+plt.show() 
